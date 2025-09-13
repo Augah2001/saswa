4b08bc68899eaf85b454a3869ddcd54f1a403718
Augah2001 <augahmups4@gmail.com>
2025-09-13 07:49:12 +0100

    final complete website

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,13 +1,16 @@
 import AdminNav from "@/components/admin/AdminNav";
+import { auth } from "@/auth";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
   return (
     <div className="flex flex-col md:flex-row h-screen bg-gray-100">
-      <AdminNav />
+      {session?.user && <AdminNav />}
       <div className="flex-1 flex flex-col overflow-hidden">
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4 md:p-8">
           {children}
